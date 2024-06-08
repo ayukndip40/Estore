df12a2f73bf2491cbb6172067029e0fe9bac2170
elvisndip95 <elvisndip9@gmail.com>
2024-06-08 14:12:55 +0200

    frontend

---
+++ src/Pages/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartItems from '../Components/CartItems/CartItems';
+
+function Cart(props) {
+    return (
+        <div>
+            <CartItems />
+        </div>
+    );
+}
+
+export default Cart;
